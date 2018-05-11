6798e47648de91e5e35989d93ab7049de425cfa9
minhnguyenqc87 <34804155+minhnguyenqc87@users.noreply.github.com>
2018-05-11 17:13:35 +0800

    Update login.rb

--- AutomationFlamingo/features/step_definitions/login.rb
+++ AutomationFlamingo/features/step_definitions/login.rb
@@ -1,26 +1,31 @@
 driver = Selenium::WebDriver.for :chrome
 wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
 
-When(/^Step 1: Navigate to Homepage$/) do #describe an event, or an action
+
+When(/^navigate to Homepage$/) do #describe an event, or an action
   driver.navigate.to "http://flamingo-nightly.symcresol.com"
 end
 
-Then (/^Step 2: Input Email$/) do #expected outcome 
+And (/^user input correct email address$/) do #describe an event, or an action
   driver.find_element(:css,'form > div:nth-child(3) > input').send_keys("minhnguyenqc87@gmail.com")
   sleep(1)
-  
 end
 
-Then (/^Step 3: Input Password$/) do #expected outcome 
+And (/^user input correct password$/) do #describe an event, or an action
   element = wait.until { driver.find_element(:css => "form > div:nth-child(4) > input") }
   element.send_keys('password')
   sleep(1)
 end
 
-Then(/^Step 4: Click to Log me In$/) do #expected outcome
+And(/^user click to Log me In button$/) do #describe an event, or an action
+  driver.find_element(:css,'form > div.buttons-w > button').click
+  sleep(2)
+end
+
+Then(/^Search Bunkering Plan Screen is shown$/) do #expected outcome 
   begin
-    driver.find_element(:css,'form > div.buttons-w > button').click
-    sleep(2)
+    element = driver.find_element(:class,"element-header").text()
+    expect("Search Bunkering Plan MEOW MEOW").to eq(element)
   ensure
     driver.quit
   end
